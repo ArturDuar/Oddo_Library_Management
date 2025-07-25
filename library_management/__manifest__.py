a4507d273464fce3605cf7168f5cccd1f86597a2
Arturo Duarte <arturduar60@gmail.com>
2025-07-25 07:42:01 -0600

    Add module properties

--- library_management/__manifest__.py
+++ library_management/__manifest__.py
@@ -1,34 +1,15 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Library Management",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "Arturo",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'name': 'Library Management',
+    'version': '1.0',
+    'author': 'Arturo',
     'category': 'Administration',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/socio_views.xml',
+        'views/libro_views.xml',
+        'views/menu_views.xml',     
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True,
+    'application': True,
 }
-
